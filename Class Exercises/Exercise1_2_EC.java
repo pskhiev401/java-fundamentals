59b053bba5589768d6b1a6edc787a1afe1272444
Patric Khiev <webdev.patric@gmail.com>
2019-06-13 22:12:36 -0700

    migrated files

---
+++ Class Exercises/Exercise1_2_EC.java
@@ -0,0 +1,24 @@
+// Patric Khiev
+// 6/12/19
+// Assignment 1.2 Extra Credit
+
+/*Write a program to take the input of amount in Dollars, and then display the denominations in  $10, $1, $ 5, 50 cents, quarter, dime, and pennies which make up the total amount.
+For example : amount=157.86
+$ 10 =15
+$ 5 =1
+$ 1 =2
+50 cents =1
+Quarters= 1
+Dime=1
+Penny=1
+*/
+
+import java.util.Scanner;
+
+public class Exercise1_2_EC {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter Dollar amount: ");
+
+  }
+}
---
+++ Class Exercises/Exercise1_3_1.java
@@ -0,0 +1,3 @@
+// Name: Patric Khiev
+// Date: 6/12/19
+// Assignment: Exercise 1.3.1
