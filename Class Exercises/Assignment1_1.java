1abc5455dc1c5031f1942c8ca9f2e3aef71593d5
Patric Khiev <webdev.patric@gmail.com>
2019-06-18 23:22:57 -0700

    needs refactoring

--- Class Exercises/Assignment1_1.java
+++ Class Exercises/Assignment1_1.java
@@ -33,12 +33,20 @@ public class Assignment1_1 {
     // Tier Calculations with Counters
     int tier1 = 0, tier2 = 0, tier3 = 0; // tiers initialized to zero
 
-    for (int i = 0; i < tripMiles; i++) { // for loop iterates through total tripMiles
-      if (tripMiles > 249 && tier1 < 100) { // tier 1 mileage
+    for (int i = 0; i < tripMiles; i++) { // for loop iterates through tripMiles
+      if (tripMiles <= 100 && tier1 == 0) { // less than 100mi
+        tier1 = tripMiles;
+        tripMiles = 0;
+      } else if (tripMiles <= 100 && tier1 <= 100) { // between 100 - 250
+        tier2 = tripMiles;
+        tripMiles = 0;
+      } else if (tripMiles >= 250) {// between 100 to 250mi
         tripMiles -= 100; // sub 100 from triptotal
         tier1 += 100; // +100 to tier1 counter
+        tier2 = tripMiles;
         System.out.printf("tripMiles: %s%n", tripMiles);
         System.out.printf("tier1: %s%n", tier1);
+        System.out.printf("tier2: %s%n", tier2);
       } else if (tripMiles > 100 && tier1 == 100 && tier2 < 150) { // tier 2 mileage
         tripMiles -= 150; // sub 150 from triptotal
         tier2 += 150; // +150 to tier2 counter
@@ -52,9 +60,16 @@ public class Assignment1_1 {
 
     switch (cabinClass) {
     case 1:
-      if (tier3 > 0 && tier2 == 0 && tier1 == 0) {
-        double firstClass3 = tier3 * 3;
-        System.out.printf("First Class fare is:%f.2%n", firstClass3);
+      if (tier3 > 0 && tier2 == 150 && tier1 == 100) { // Trips for beyond 250mi
+        double firstClass3 = ((tier1 * 3) + (tier2 * 2.5) + (tier3 * 2));
+        System.out.printf("First Class fare: %-6.2f %n", firstClass3);
+      } else if (tier2 == 150 && tier1 == 100) { // Trips between 100mi - 250 mi
+        double firstclass2 = ((tier2 * 2.5) + (tier3 * 2));
+        System.out.printf("First Class fare: %-6.2f %n", firstclass2);
+
+      } else if (tier1 <= 100) {
+        double firstClass1 = tier1 * 3;
+        System.out.printf("First Class fare: %-6.2f %n", firstClass1);
       }
       // case 2: if (tier3 == 0){
 
