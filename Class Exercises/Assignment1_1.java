779ec0bbdded02a83419f248f3ac1306e73b97e6
Patric Khiev <webdev.patric@gmail.com>
2019-06-19 10:18:28 -0700

    solution: removed for-loop & logic added to switch cases

--- Class Exercises/Assignment1_1.java
+++ Class Exercises/Assignment1_1.java
@@ -25,55 +25,78 @@ public class Assignment1_1 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.println("Input total miles of your destination: ");
-    int tripMiles = input.nextInt();
+    float tripMiles = input.nextInt();
     System.out.printf("Select desired passenger class:%n1 = First Class, 2= Second Class, 3 = Third Class%n");
     int cabinClass = input.nextInt();
-    System.out.printf("total miles: %d%n", tripMiles);
-
-    // Tier Calculations with Counters
-    int tier1 = 0, tier2 = 0, tier3 = 0; // tiers initialized to zero
-
-    for (int i = 0; i < tripMiles; i++) { // for loop iterates through tripMiles
-      if (tripMiles <= 100 && tier1 == 0) { // less than 100mi
-        tier1 = tripMiles;
-        tripMiles = 0;
-      } else if (tripMiles <= 100 && tier1 <= 100) { // between 100 - 250
-        tier2 = tripMiles;
-        tripMiles = 0;
-      } else if (tripMiles >= 250) {// between 100 to 250mi
-        tripMiles -= 100; // sub 100 from triptotal
-        tier1 += 100; // +100 to tier1 counter
-        tier2 = tripMiles;
-        System.out.printf("tripMiles: %s%n", tripMiles);
-        System.out.printf("tier1: %s%n", tier1);
-        System.out.printf("tier2: %s%n", tier2);
-      } else if (tripMiles > 100 && tier1 == 100 && tier2 < 150) { // tier 2 mileage
-        tripMiles -= 150; // sub 150 from triptotal
-        tier2 += 150; // +150 to tier2 counter
-        System.out.printf("tripMiles: %s%n", tripMiles);
-        System.out.printf("tier2: %s%n", tier2);
-      } else if (tripMiles > 0 && tier1 == 100 && tier2 == 150) { // mileage beyond tier1 & tier2
-        tier3 = tripMiles; // assigns tripmiles to tier3 counter
-      }
-    }
-    System.out.println(tier3);
 
     switch (cabinClass) {
+
     case 1:
-      if (tier3 > 0 && tier2 == 150 && tier1 == 100) { // Trips for beyond 250mi
-        double firstClass3 = ((tier1 * 3) + (tier2 * 2.5) + (tier3 * 2));
-        System.out.printf("First Class fare: %-6.2f %n", firstClass3);
-      } else if (tier2 == 150 && tier1 == 100) { // Trips between 100mi - 250 mi
-        double firstclass2 = ((tier2 * 2.5) + (tier3 * 2));
-        System.out.printf("First Class fare: %-6.2f %n", firstclass2);
-
-      } else if (tier1 <= 100) {
-        double firstClass1 = tier1 * 3;
-        System.out.printf("First Class fare: %-6.2f %n", firstClass1);
+      if (tripMiles <= 100) {
+        System.out.println(tripMiles * 3.0);
+        break;
+      } else if (tripMiles > 100 && tripMiles <= 250) {
+        tripMiles -= 100;
+        float tier1 = 100;
+        float tier2 = tripMiles;
+        double tripCost = (tier1 * 3) + (tier2 * 2.5);
+        System.out.println(tripCost);
+        break;
+      } else if (tripMiles > 250) {
+        tripMiles -= 100;
+        float tier1 = 100;
+        tripMiles -= 150;
+        float tier2 = 150;
+        float tier3 = tripMiles;
+        double tripCost = (tier1 * 3) + (tier2 * 2.5) + (tier3 * 2);
+        System.out.println(tripCost);
+        break;
       }
-      // case 2: if (tier3 == 0){
-
-      // }
+    case 2:
+      if (tripMiles <= 100) {
+        System.out.println(tripMiles * 2.5);
+        break;
+      } else if (tripMiles > 100 && tripMiles <= 250) {
+        tripMiles -= 100;
+        float tier1 = 100;
+        float tier2 = tripMiles;
+        double tripCost = (tier1 * 2.5) + (tier2 * 1.5);
+        System.out.println(tripCost);
+        break;
+      } else if (tripMiles > 250) {
+        tripMiles -= 100;
+        float tier1 = 100;
+        tripMiles -= 150;
+        float tier2 = 150;
+        float tier3 = tripMiles;
+        double tripCost = (tier1 * 2.5) + (tier2 * 1.5) + (tier3 * 1);
+        System.out.println(tripCost);
+        break;
+      }
+    case 3:
+      if (tripMiles <= 100) {
+        System.out.println(tripMiles * 2);
+        break;
+      } else if (tripMiles > 100 && tripMiles <= 250) {
+        tripMiles -= 100;
+        float tier1 = 100;
+        float tier2 = tripMiles;
+        double tripCost = (tier1 * 2) + (tier2 * 1);
+        System.out.println(tripCost);
+        break;
+      } else if (tripMiles > 250) {
+        tripMiles -= 100;
+        float tier1 = 100;
+        tripMiles -= 150;
+        float tier2 = 150;
+        float tier3 = tripMiles;
+        double tripCost = (tier1 * 2) + (tier2 * 1) + (tier3 * 0.5);
+        System.out.println(tripCost);
+        break;
+      }
+    default:
+      System.out.println("Error, invalid input");
     }
+    System.out.println("End of Program");
   }
 }
