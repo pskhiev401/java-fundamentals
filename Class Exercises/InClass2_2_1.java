e0a53f4325df817fecc99f1dab89de3573819575
Patric Khiev <webdev.patric@gmail.com>
2019-06-25 18:48:47 -0700

    solved sum of series & armstrong

--- Class Exercises/InClass2_2_1.java
+++ Class Exercises/InClass2_2_1.java
@@ -12,7 +12,7 @@ Write a menu driven program which uses a method for each of the following operat
 6. Palindrome Test
 7. Exponent Evaluation
 
-(Note : The user should be allowed to repeat the operations as long as he wants to. Use appropriate number of parameters and return type for each method.)
+The user should be allowed to repeat the operations as long as he wants to. Use appropriate number of parameters and return type for each method
 */
 
 import java.util.Scanner;
@@ -22,7 +22,7 @@ public class InClass2_2_1 {
     Scanner keyboard = new Scanner(System.in);
     int userChoice;
     do {
-      System.out.println("Choose a program:");
+      System.out.println("Choose a Program:");
       System.out.println("1. Sum of Series");
       System.out.println("2. Armstrong Number");
       System.out.println("3. Prime/Composite Test");
@@ -33,23 +33,87 @@ public class InClass2_2_1 {
       System.out.println("7. Exponent Evaluator");
       System.out.println("8. Quit Program");
       userChoice = keyboard.nextInt();
-      System.out.println(userChoice);
-
+      // System.out.println(userChoice);
       switch (userChoice) {
       case 1:
-        // case 2:
-        // case 3:
-        // case 4:
-        // case 5:
-        // case 6:
-        // case 7:
-
+        System.out.println("Enter a number: ");
+        int num1 = keyboard.nextInt();
+        // System.out.println(num);
+        SumSeries(num1);
+        break;
+      case 2:
+        System.out.println("Enter a number: ");
+        int num2 = keyboard.nextInt();
+        // System.out.println(num);
+        armstrongNum(num2);
+        break;
+      case 3:
+        System.out.println("Enter a number: ");
+        int num3 = keyboard.nextInt();
+        PrimeNum(num3);
+        break;
+      // case 4:
+      // System.out.println("Enter a number: ");
+      // int num4 = keyboard.nextInt();
+      // MultiplyNum_Tenx(num4);
+      // break;
+      // case 5:
+      // System.out.println("Enter a number: ");
+      // int num5 = keyboard.nextInt();
+      // DigitDetails(num5);
+      // break;
+      // case 6:
+      // System.out.println("Enter a number: ");
+      // int num6 = keyboard.nextInt();
+      // Palindrome(num6);
+      // break;
+      // case 7:
+      // System.out.println("Enter a number: ");
+      // int num7 = keyboard.nextInt();
+      // factorial(num7);
+      // break;
+      // case 8:
+      // System.out.println("Enter a base: ");
+      // int base = keyboard.nextInt();
+      // System.out.println("Enter an exponent: ");
+      // int exponent = keyboard.nextInt();
+      // ExponentEval(base, exponent);
+      // break;
+      case 9:
+        System.out.println("Cya Later....");
+        return;
       }
-
-    } while (userChoice != 8);
+    } while (userChoice != 9);
   }
-  public static int SumSeries(int n){
-    return sum;
+
+  public static void SumSeries(int number) {
+    System.out.print("2^2"); // Prints our first set in the series
+    int sum = 4; // initialized to 4, because 2^2 = 4
+    for (int x = 4; x <= 2 * number; x += 2) { // determines sum & prints nth sets concatnated
+      System.out.print(" + " + x + "^" + "2");
+      sum += x * x;
+    }
+    System.out.println();
+    System.out.println("Sum of terms= " + sum);
+  }
+
+  public static void armstrongNum(int number) {
+    int result = 0;
+    int temp; // temp holds original number value
+    int num;
+
+    temp = number;
+    while (number > 0) {
+      num = number % 10;
+      number = number / 10;
+      result += (num * num * num);
+    }
+    // System.out.println(result);
+    if (result == temp) { // comparing the original number with the result
+      System.out.println("This is an armstrong number.");
+    } else {
+      System.out.println("This is not an armstrong number.");
+    }
   }
 
 }
